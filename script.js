6ce22f7a07b8ebfeea35f535b322d052b0a44cf5
Sharmilaj27 <145528865+Sharmilaj27@users.noreply.github.com>
2025-10-24 16:36:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Optional: You can randomize PAN number or Name
+const panNumber = "ABCDE1234F";
+document.querySelector(".details").innerHTML += `<p><strong>Generated:</strong> ${new Date().toLocaleDateString()}</p>`;
